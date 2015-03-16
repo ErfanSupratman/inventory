503839d3adb2745138417f15c438d8a20b2cc1fd
Yunus Bahari <yunus11@mhs.if.its.ac.id>
2015-03-16 11:42:48 +0700

    ADD MODULE cetak

--- application/views/cetak.php
+++
@@ -1,8 +0,0 @@
-<html>
-  <head>
-    <title>Tutorial: HelloWorld</title>
-  </head>
-  <body>
-    <h1>Hello World Tutorial</h1>
-  </body>
-</html>
--- application/views/dashboard/adminmenu.php
+++ application/views/dashboard/adminmenu.php
@@ -13,13 +13,18 @@
       <li class="start active "> <a href="<?php echo base_url(); ?>dashboard/lokasi"><i class="fa fa-key fa-fw"></i><span class="title">Lokasi</span> <span class="badge badge-disable pull-right"></span></a> </li>
       <!--<li class="start active "> <a href="<?php echo base_url(); ?>dashboard/merk"><i class="fa fa-refresh fa-spin"></i><span class="title">Merk</span> <span class="badge badge-disable pull-right"></span></a> </li>-->
       <li class="start active "> <a href="<?php echo base_url(); ?>dashboard/user"><i class="fa fa-users fa-fw"></i><span class="title">Pengguna</span> <span class="badge badge-disable pull-right"></span></a> </li>
+      <li class="start active "> <a href="<?php echo base_url(); ?>dashboard/cetak"><i class="fa fa-print fa-fw"></i><span class="title">Cetak</span> <span class="badge badge-disable pull-right"></span></a> </li>
+    </ul>
     </ul>
 
   </div>
   </div>
-  <a href="#" class="scrollup"></a>
+  </div>
+  </div>
+  <a cetak class="cetak</a>
+   <div class="cetak-widget">    
    <div class="footer-widget">		
-	<div class="progress transparent progress-small no-radius no-margin">
+	<div class="progress transparent progress-small no-cetak no-margin">
 		<div data-percentage="79%" class="progress-bar progress-bar-success animate-progress-bar" ></div>		
 	</div>
   </div>
